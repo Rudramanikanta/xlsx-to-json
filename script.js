6b3e56c5ac3b585ea3d3a1c7d0510f046a784237
Your NameRudramanikanta <rudramanikanta02@gmail.com>
2023-02-23 20:01:06 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+
+let selectedFile;
+console.log(window.XLSX);
+document.getElementById('input').addEventListener("change", (event) => {
+    selectedFile = event.target.files[0];
+})
+
+
+
+
+document.getElementById('click').addEventListener("click", () => {
+  
+    if(selectedFile){
+        let fileReader = new FileReader();
+        fileReader.readAsBinaryString(selectedFile);
+        fileReader.onload = (event)=>{
+         let data = event.target.result;
+         let workbook = XLSX.read(data,{type:"binary"});
+         console.log(workbook);
+         workbook.SheetNames.forEach(sheet => {
+              let rowObject = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[sheet]);
+              console.log(rowObject);
+              document.getElementById("jsondata").innerHTML = JSON.stringify(rowObject,undefined,4)
+         });
+        }
+    }
+});
